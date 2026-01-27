cdfbd1786c7e4de2f0a8c0823f1bb9fea15b7080
ThembelihleQueeneth <malukathembelihle95@gmail.com>
2026-01-27 21:11:26 +0200

    Working on the content of Admin Screen

--- src/pages/AdminLogin.tsx
+++ src/pages/AdminLogin.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import Header from '../components/Header'
 
 export default function AdminLogin() {
   return (
-    <div>AdminLogin</div>
+    <div>
+      <Header/>
+      <label htmlFor="">Email Adrress</label><br />
+      <input type="text" placeholder='Enter email here' /><br />
+      <label htmlFor="">Password</label><br />
+      <input type="password" placeholder='Enter password here' /><br />
+      <button>Login</button>
+    </div>
   )
 }
